10ecd0a27b664f462ba7314a2c40b25eca6d4a77
Carlos Narváez <narvaezcarlos88@gmail.com>
2026-05-11 01:01:25 +0000

    update routes.jsx

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -10,6 +10,9 @@ import { Home } from "./pages/Home";
 import { Single } from "./pages/Single";
 import { Demo } from "./pages/Demo";
 
+import { Signup } from "./pages/Signup.jsx";
+import { Login } from "./pages/Login.jsx";
+
 export const router = createBrowserRouter(
     createRoutesFromElements(
     // CreateRoutesFromElements function allows you to build route elements declaratively.
@@ -25,6 +28,8 @@ export const router = createBrowserRouter(
         <Route path= "/" element={<Home />} />
         <Route path="/single/:theId" element={ <Single />} />  {/* Dynamic route for single items */}
         <Route path="/demo" element={<Demo />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
       </Route>
     )
 );
